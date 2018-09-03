5325091342e58a16f8c1925a457f719394ac208e
Parker Wouters <pepper5319@gmail.com>
2018-09-03 14:37:15 -0400

    Update settings.py

--- listr/settings.py
+++ listr/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wn^i-4lir!_l+412p@5h8zu6qiqm^jr31zk3dzjjqyl1xnmsr7'
+SECRET_KEY = '-----------'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
