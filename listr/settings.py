1a65897d881b3b4a83977ff759abaa550997f1cd
pepper5319 <pepper5319@gmail.com>
2018-09-23 19:03:38 -0400

    Updated to Material Design

--- listr/settings.py
+++ listr/settings.py
@@ -63,6 +63,8 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+#CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://localhost:3000/',
