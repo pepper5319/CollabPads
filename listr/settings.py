af3ed68ae8b040f529dcf349b9319e4dfe337cb2
pepper5319 <pepper5319@gmail.com>
2018-09-27 16:07:54 -0400

    Fixed Heroku DB Bug

--- listr/settings.py
+++ listr/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import api
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -162,3 +163,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 STATICFILES_DIRS = [
     #os.path.join(BASE_DIR, 'build/static/'),
 ]
+django_heroku.settings(locals())
